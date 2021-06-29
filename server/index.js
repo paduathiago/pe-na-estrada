a1175f77a4cdd542aa5c73256190aee0208ebfaf
Artur <gaspar.artur@yahoo.com.br>
2021-06-29 19:37:30 -0300

    Arrumados erros iniciais de estilo segundo ESLint.

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const web = require('./config/express-config');
 
-web.get('/', (req,res) => res.send('<h1>Testando site!<\h1>'));
+web.get('/', (req, res) => res.send('<h1>Testando site!<\h1>'));
 
-web.listen(3001,'localhost',()=>console.log('Servidor ligado!'));
+web.listen(3001, 'localhost', ()=>console.log('Servidor ligado!'));
