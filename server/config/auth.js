f27bf4d13c290dbfa83e79cd530d7cd666b71223
Artur <gaspar.artur@yahoo.com.br>
2021-07-07 20:31:05 -0300

    Adicionada estrategia local de autenticação.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,2 +1,34 @@
-//  Para coisas de autenticação
+const Passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../viajantes/model/Viajante');
+
+Passport.use(
+  'login',
+  new LocalStrategy(
+    {
+      usernameField: 'email',
+      passwordField: 'senha',
+      session: false,
+    },
+    async (email, senha, done) => {
+      try {
+        const user = await User.findOne({
+          where: {email: email},
+        });
+        if (!user) {
+          throw new Error('E-mail ou senha incorretos!');
+        }
+
+        const senhaBateu = (senha === user.senha);
+        if (!senhaBateu) {
+          throw new Error('E-mail ou senha incorretos!');
+        }
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    },
+  ),
+);
 
