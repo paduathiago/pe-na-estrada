ae432b296e099ea1dee9bc2848e58e248bb80a4a
Artur <gaspar.artur@yahoo.com.br>
2021-07-07 20:53:03 -0300

    Configurada estratégia JWT no passport.

--- server/config/auth.js
+++ server/config/auth.js
@@ -1,5 +1,6 @@
 const Passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
 const User = require('../viajantes/model/Viajante');
 
 Passport.use(
@@ -32,3 +33,29 @@ Passport.use(
   ),
 );
 
+function tiraBiscoito(req) {
+  let token = null;
+
+  if (req && req.cookies) {
+    token=req.cookies['jwt'];
+  }
+
+  return token;
+}
+
+Passport.use(
+  'jwt',
+  new JwtStrategy(
+    {
+      secretOrKey: process.env.SECRET_KEY,
+      jwtFromRequest: tiraBiscoito,
+    },
+    async (jwtPayload, done)=>{
+      try {
+        return done(null, jwtPayload.user);
+      } catch (error) {
+        return (done, false);
+      }
+    },
+  ),
+);
