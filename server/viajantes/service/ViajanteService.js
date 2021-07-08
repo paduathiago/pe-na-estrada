8b7b7086bc7edd2513777540194d05bc96f8f8b9
Artur <gaspar.artur@yahoo.com.br>
2021-07-08 20:49:28 -0300

    Adicionado serviço certo de criar viajante.

--- server/viajantes/service/ViajanteService.js
+++ server/viajantes/service/ViajanteService.js
@@ -1,8 +1,19 @@
 const Viajante= require('../model/Viajante');
+const bcrypt= require('bcrypt');
 
 class ViajanteService {
   async createViajante(viajante) {
-    await Viajante.create(viajante);
+    try {
+      // 'Quantas vezes vai repetir a encriptação'...
+      const saltRounds=10;
+      viajante.senhaHash= await bcrypt.hash(viajante.senha, saltRounds);
+      delete viajante.senha;
+      // Gambiarra minha aqui tbm
+      viajante.senhaSalt='';
+      await Viajante.create(viajante);
+    } catch (error) {
+      throw error;
+    }
   }
   async getAllViajantes() {
     return await Viajante.findAll({raw: true});
