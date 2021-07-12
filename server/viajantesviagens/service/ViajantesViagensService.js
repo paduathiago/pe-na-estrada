32c399e79b93c6090d8d17237e2467a44da3f038
Gabriel Teixeira <Gteixeiraca@gmail.com>
2021-07-12 19:33:18 -0300

    Fiz os services da Associação

---
+++ server/viajantesviagens/service/ViajantesViagensService.js
@@ -0,0 +1,9 @@
+const ViajanteViagens = require('../model/ViajanteViagens');
+
+class ViajanteViagensService{
+    async createViajanteViagem(viajanteviagens){
+        await ViajanteViagens.create(viajanteviagens);
+    }
+} 
+
+module.exports = new ViajanteViagensService;
