b7162aa2657ee760275a7a4e42c984e4faf02136
Artur <gaspar.artur@yahoo.com.br>
2021-07-20 22:34:18 -0300

    Batata frita completada!

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
+import logoBatata from './logoBatata.png';
 import './App.css';
+import React from 'react'
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id:0,
+      colors:["#ff0000","#aaaa00","#00ff00","#00aaaa","#0000ff"]
+    };
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      id:(this.state.id+1)%5
+    });
+  }
+  render() {
+    return (
+      <div>
+        <font color={this.state.colors[this.state.id]}>BATATA FRITA!.</font>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logoBatata} className="App-logo" alt="logo" />
+        <Clock></Clock>
       </header>
     </div>
   );
