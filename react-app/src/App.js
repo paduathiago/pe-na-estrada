ad322d7cee3a20b8c0d851df1cc053eb251c1196
Artur <gaspar.artur@yahoo.com.br>
2021-07-21 20:09:46 -0300

    Preparada estrutura inicial da Home.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import axios from 'axios'
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import Home from './components/Home/Home';
 
 function App() {
-
+  axios.defaults.baseURL=process.env.REACT_APP_API_URL;
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ react-app/src/components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home(){
+    return(
+        <div className="Home">
+            <div className="Header">
+                <div className="flex-title">
+                    Titulo.
+                </div>
+            </div>
+        </div>
+    )
+}
