dc42e3b0a4062187a5a175215a4bd61318f4a3b8
Artur <gaspar.artur@yahoo.com.br>
2021-07-23 09:29:05 -0300

    Adicionado controle de componentes no form de login.

--- react-app/src/components/Login/Form/Form.jsx
+++ react-app/src/components/Login/Form/Form.jsx
@@ -1,7 +1,18 @@
 import Logo from '../../../assets/logo.png'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Form() {
+    const [email,setEmail] = useState('')//As aspas são o estado inicial!
+    const [senha, setSenha] = useState('')
+
+    function handleEmailChange(event){
+        setEmail(event.target.value)
+    }
+    function handleSenhaChange(event){
+        setSenha(event.target.value)
+    }
+
     return (
       <div className="Form">
         <form method="POST">
@@ -15,13 +26,14 @@ export default function Form() {
             <div id="Email">
               <label htmlFor='email'><p>Login:</p></label>
               <input type='text' placeholder="Digite seu email" name="email"
-                required
+                required onChange={handleEmailChange} value={email}
                 />
             </div>
             <div id="Password">
               <input id="form-bottom" type="password"
                 placeholder="Digite sua senha"
                 name="password" required
+                onChange={handleSenhaChange} value={senha}
                 />
               <span className="password">
                 <Link to="/">
