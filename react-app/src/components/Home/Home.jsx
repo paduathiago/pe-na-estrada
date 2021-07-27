c3f719cef655b8a1a445404be437eed7b55add03
Artur <gaspar.artur@yahoo.com.br>
2021-07-27 16:39:15 -0300

    Protótipo inical da Home preparado.

--- react-app/src/components/Home/Home.jsx
+++ react-app/src/components/Home/Home.jsx
@@ -2,13 +2,24 @@ import React from 'react'
 import './Home.css'
 import Menu from '../Menu/Menu'
 import Footer from '../Footer/Footer'
+import Logo from '../../assets/logo.png'
 
 export default function Home(){
     return(
         <div className="Home">
             <Menu />
-            <div className="Conteudo">
-                Conteudo.
+            <div className="row m-5">
+                <figure>
+                    <img src={Logo}
+                    alt="Logo"
+                    width="30%"
+                    padding="0"
+                    className="logoMovel"
+                    />
+                    <figcaption className="descLogo">
+                        Rotas Worldwide
+                    </figcaption>
+                </figure>
             </div>
             <Footer />
         </div>
