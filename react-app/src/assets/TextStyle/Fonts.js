f2cee554c7dbc2b28a3742a17709bd05dff14e1f
Artur <gaspar.artur@yahoo.com.br>
2021-07-27 20:37:40 -0300

    Adicionadas fontes desejadas

---
+++ react-app/src/assets/TextStyle/Fonts.js
@@ -0,0 +1,13 @@
+let fontes={
+    textos:'Bitter',
+    subTitulo:'Tinos',
+    titulo:'Lato'
+}
+
+export {fontes}
+
+/**
+ * font-family: 'Bitter', serif;
+ * font-family: 'Lato', sans-serif;
+ * font-family: 'Tinos', serif;
+ */
