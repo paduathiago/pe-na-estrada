4562296455886d280949bc42883df6f3ea87ddf3
Gabriel Teixeira <Gteixeiraca@gmail.com>
2021-07-28 04:33:48 -0300

    Menu Provisório

--- react-app/src/components/Menu/Menu.jsx
+++ react-app/src/components/Menu/Menu.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './Menu.css'
 
 //MENU TEMPORARIO! ARRUMAR!!!
 export default function Menu(){
     return(
         <div className="Menu">
-            <Link to="/login">
-                <button className="login-button">
-                    Login
-                </button>
-            </Link>
             <Link to='/'>
                 <button className="home-button">
                     Home
+                </button>
+            </Link>
+            <Link to="/login">
+                <button className="login-button">
+                    Login
                 </button>
             </Link>
         </div>
