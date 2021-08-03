32f88ee47e4a89e516f642d881d124ff1d0c9557
Artur <gaspar.artur@yahoo.com.br>
2021-08-03 20:46:05 -0300

    Criado validador de objetos.

---
+++ server/middlewares/data-validators.js
@@ -0,0 +1,12 @@
+function objectValidator(obj, keys) {
+  return function(req, res, next) {
+    Object.keys(req[obj]).forEach((key)=>{
+      if (keys.indexOf(key)===-1) {// Deleta chaves que não deveriam estar lá
+        delete key;
+      }
+    });
+    next();
+  };
+}
+
+module.exports=objectValidator;
