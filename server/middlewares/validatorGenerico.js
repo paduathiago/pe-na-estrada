26c977157049ab5e741ebf1d84725582c632f110
Artur <gaspar.artur@yahoo.com.br>
2021-08-03 21:27:56 -0300

    Adicionado validador genérico de dados.

---
+++ server/middlewares/validatorGenerico.js
@@ -0,0 +1,15 @@
+const {validationResult} = require('express-validator');
+
+function validate(validations) {
+  return async function(req, res, next) {
+    for (const val of validations) {
+      await val.run(req);
+    }
+    const result= validationResult(req);
+    if (!result.isEmpty()) {
+      res.status(400).send(result.errors[0].msg);
+    } else next();
+  };
+}
+
+module.exports= validate;
