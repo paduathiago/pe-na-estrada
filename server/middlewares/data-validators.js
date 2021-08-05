e14b76429acfa4f3d295fba80bb0e5f0835070f4
Artur <gaspar.artur@yahoo.com.br>
2021-08-05 20:13:41 -0300

    Arrumado filtro de objetos.

--- server/middlewares/data-validators.js
+++ server/middlewares/data-validators.js
@@ -2,7 +2,7 @@ function objectValidator(obj, keys) {
   return function(req, res, next) {
     Object.keys(req[obj]).forEach((key)=>{
       if (keys.indexOf(key)===-1) {// Deleta chaves que não deveriam estar lá
-        delete key;
+        delete req[obj][key];
       }
     });
     next();
