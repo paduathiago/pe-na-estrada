d2b425728d4795855dcc0931f110e6aa95cadf7f
Artur <gaspar.artur@yahoo.com.br>
2021-08-05 20:32:21 -0300

    Atualizado service de atualizar viagem pra poder por ou tirar viajantes dela.

--- server/viagens/service/ViagemService.js
+++ server/viagens/service/ViagemService.js
@@ -19,7 +19,31 @@ class ViagemService {
     return await Viagem.findByPk(id, {raw: true});
   }
   async updateViagem(id, body) {
+    let addViajantes
+    let remViajantes
+    if(body.addViajantes){
+      addViajantes=JSON.parse(body.addViajantes)
+      delete body.addViajantes
+    }
+    if(body.remViajantes){
+      remViajantes=JSON.parse(body.remViajantes)
+      delete body.remViajantes
+    }
     await Viagem.update(body, {where: {id: id}});
+    const vi=await Viagem.findByPk(id)
+    if(addViajantes){
+      for(v in addViajantes){
+        const viaj=await Viajante.findByPk(addViajantes[v]);
+        await vi.addViajante(viaj)
+      }
+    }
+    if(remViajantes){
+      for(v in remViajantes){
+        const viaj=await Viajante.findByPk(remViajantes[v]);
+        await vi.removeViajante(viaj)
+      }
+    }
+
   }
   async deleteViagem(id) {
     await Viagem.destroy({where: {id: id}});
