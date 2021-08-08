48d58b72e93ac3cc017d6a99bedfa07f9ae6c9a6
Artur <arthur.gaspar2011@gmail.com>
2021-08-07 21:15:08 -0300

    Atualizado rota me para utilizar o estado global de usuário logado.

--- react-app/src/components/Me/Me.jsx
+++ react-app/src/components/Me/Me.jsx
@@ -2,15 +2,7 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Image } from 'react-bootstrap';
 
-export default function Me() {
-    const [user, setUser] = useState(false);
-    useEffect(() => {
-      axios.get('/me')
-        .then( (res) => setUser(res.data) )
-        .catch( (err) => console.log(err.response) )
-    }, []);
-
-
+export default function Me({user}) {
     return (
         <div>
             <p>{user.nome}</p>
