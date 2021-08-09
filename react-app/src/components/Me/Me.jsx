b57179ad4b7995ea576efdf7ca4b92e86af1d9a9
Artur <arthur.gaspar2011@gmail.com>
2021-08-09 19:18:21 -0300

    Adicionada lista de viagens no /me.

--- react-app/src/components/Me/Me.jsx
+++ react-app/src/components/Me/Me.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Image } from 'react-bootstrap';
+import axios from 'axios';
+import ListaInline from '../ListaInline/ListaInline'
 
 export default function Me({user}) {
-    return (
-        <div>
-            <p>{user.nome}</p>
-            <Image width="400px" height="400px" src={user.imagemPerfil} roundedCircle />
-            <p>{user.introducao}</p>
-        </div>
-    )
+  const [viagens, setViagens] = useState(false);
+  useEffect(() => {
+    axios.get(`/viajantes/${user.id}`)
+      .then( (res) => {
+      setViagens(res.data.Viagens);
+      })
+      .catch( (err) => console.log(err.response) )
+    },[user.id]);
+  return (
+    <div>
+      <p>{user.nome}</p>
+      <Image width="400px" height="400px" src={user.imagemPerfil} roundedCircle />
+      <p>{user.introducao}</p>
+      <div className="viagens">
+        <ListaInline itens={viagens} ifNone="Nenhuma viagem cadastrada."
+          itensEnvolvidos="Viagens envolvido(a)" tituloAttr="localizacao" 
+          rota="viagens"/>
+      </div>
+    </div>
+
+  )
 }
