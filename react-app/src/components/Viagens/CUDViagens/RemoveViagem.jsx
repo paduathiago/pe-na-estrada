b452b2919b76bff7fa30fbea564d048da36b8223
Artur <arthur.gaspar2011@gmail.com>
2021-08-10 13:20:02 -0300

    Renomeado CUD de viagens por não ter o Delete.

--- react-app/src/components/Viagens/CUDViagens/RemoveViagem.jsx
+++
@@ -1,4 +0,0 @@
-
-export default function RemoveViagem(){
-    return <div>CERTEZA QUE QUER REMOVER?</div>
-  }
--- react-app/src/components/Viagens/Viagens.jsx
+++ react-app/src/components/Viagens/Viagens.jsx
@@ -5,9 +5,8 @@ import {
 
 import Viagem from './Viagem/Viagem'
 import ViagensList from './ViagensList/ViagensList';
-import CreateViagem from './CUDViagens/CreateViagem'
-import RemoveViagem from './CUDViagens/RemoveViagem'
-import UpdateViagem from './CUDViagens/UpdateViagem';
+import CreateViagem from './UCViagens/CreateViagem'
+import UpdateViagem from './UCViagens/UpdateViagem';
 
 import './Viagens.css';
 
@@ -19,7 +18,6 @@ export default function Viagens({user}) {
       <Route exact path={`/viagens/`} component={()=>ViagensList({user})}/>
       <Route exact path={`/viagens/criar`} component={()=>CreateViagem({user})}/>
       <Route path={`/viagens/:id/editar`} component={({match})=>UpdateViagem({match,user})}/>
-      <Route path={`/viagens/:id/remover`} component={({match})=>RemoveViagem({match,user})}/>
       <Route path={`/viagens/:id`} component={({match})=>Viagem({match,user})}/>
     </Switch>
   </div>
