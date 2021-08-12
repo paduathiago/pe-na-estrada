a993efb529162273915a9922c5a7e285771080e5
Artur <arthur.gaspar2011@gmail.com>
2021-08-12 15:04:10 -0300

    Adicionado protótipo de  dropdown list pra lista de viajantes

---
+++ react-app/src/components/Viajantes/DropdownViajantesList/DropdownViajantesList.jsx
@@ -0,0 +1,40 @@
+import { useState} from 'react';
+
+export default function DropdownViajantesList({lista,setViajantes,viajantes}){
+  const [idsFiltrados,setIdsFiltrados]=useState('')
+  const [nomesFiltrados,setNomesFiltrados]=useState([])
+  function handleChange(setProp){
+    return (event)=>setProp(event.target.value)
+  }
+  function handleListaViajantes(event){
+    let nomesViajantes = lista.map(value => value.nome)
+    let idsViajantes = lista.map(value => value.id)
+    let nomesId = {}
+    for (let index = 0; index < nomesViajantes.length; index++) {
+      nomesId[nomesViajantes[index]] = idsViajantes[index]
+    }
+    let nomesFiltrados = nomesViajantes.filter(el => el.toLowerCase().indexOf(event.target.value.toLowerCase()) > -1);
+    
+    let idsViajantesSelecionados = [];
+    for (let index = 0; index < nomesFiltrados.length; index++) {
+      idsViajantesSelecionados.push(nomesId[nomesFiltrados[index]])
+    }
+    console.log(idsViajantesSelecionados)
+    setNomesFiltrados(nomesFiltrados)
+    setIdsFiltrados(idsViajantesSelecionados)
+  }
+
+  let loadedViajantes = [];
+  const viajantesToOptions = (element, index) => <option value={element} />
+  if(nomesFiltrados) loadedViajantes = nomesFiltrados.map(viajantesToOptions)
+  return <div>
+      <input list='viajantesList' placeholder="Digite a lista de viajantes" name="viajantes"
+        required onChange={handleChange(setViajantes)} value={viajantes} 
+        onInput={handleListaViajantes}
+        />
+      <datalist id="viajantesList">
+        {loadedViajantes}
+      </datalist>
+    </div>
+
+}
